9b117c137d3e6477ef06150cadefbea722e21050
1Oscar089 <mejiaoscar17.71@gmail.com>
2025-12-02 19:49:26 -0600

    act.

---
+++ js/navbar.js
@@ -0,0 +1,16 @@
+let menu = [
+    {nombre: "Inicio", link: "../index.html"},
+    {nombre: "Linux consola", link: "../pages/linux_consola.html"},
+];
+let nav = document.getElementById("nav");
+const ul = document.createElement("ul");
+menu.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = item.nombre;
+    li.addEventListener("click", () => {
+        window.location.href = item.link;
+    });
+    ul.appendChild(li);
+});
+nav.textContent = "&#8801;"
+nav.appendChild(ul);
